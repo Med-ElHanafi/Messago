f43c333ba1433e9e8767d8b6366de7b2d440e2e7
elhanafi <med.y.elhanafi@gmail.com>
2024-12-31 22:56:13 +0100

    Initial Commit

---
+++ Messago/MessagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagoApp.swift
+//  Messago
+//
+//  Created by Mohamed El hanafi on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MessagoTests/MessagoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MessagoTests.swift
+//  MessagoTests
+//
+//  Created by Mohamed El hanafi on 31/12/2024.
+//
+
+import Testing
+@testable import Messago
+
+struct MessagoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
