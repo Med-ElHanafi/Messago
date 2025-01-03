c842effa065151bb8ea9daaaef7d27c7250072ca
elhanafi <med.y.elhanafi@gmail.com>
2025-01-03 06:00:14 +0100

    add focus + refactor

--- Messago/UI/LoginView.swift
+++ Messago/UI/LoginView.swift
@@ -8,64 +8,108 @@
 import SwiftUI
 
 struct LoginView: View {
+    @FocusState private var focusedInput: Field?
+    
     var body: some View {
         ScrollView {
-            VStack {
-                Image(.logo)
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .padding(50)
-                    .background(.accent)
-            }
-            .frame(maxWidth: .infinity)
-            .background(.accent)
-            
+            logoBanner
             
             VStack(spacing: 30) {
                 Text("Login")
                     .font(.largeTitle)
                     .fontWeight(.medium)
-                    .padding(.vertical, 60)
+                    .padding(.vertical)
                 
-                CustomTextField(
-                    text: .constant(""),
-                    title: "Email",
-                    placeholder: "example@email.com"
-                )
+                emailTextField
+                    .focused($focusedInput, equals: .email)
                 
-                CustomTextField(
-                    text: .constant(""),
-                    title: "Password",
-                    placeholder: "•••••••••••",
-                    secure: true
-                )
+                passwordTextField
+                    .focused($focusedInput, equals: .password)
                 
-                Button("Login") {
-                    
-                }
-                .padding(.vertical, 20)
-                .foregroundStyle(.white)
-                .frame(maxWidth: .infinity)
-                .background(.accent)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+                loginButton
                 
-                Button("Don't have an account? Sign up") {
-                    
-                }
-                .fontWeight(.medium)
-                .frame(maxWidth: .infinity)
+                signupButton
             }
             .padding()
-            .frame(maxHeight: .infinity)
             .background(.white)
             .clipShape(CustomRoundedRectangle(corner: .topLeft, radius: 50))
             .offset(y: -60)
         }
-        .frame(maxHeight: .infinity)
-        .ignoresSafeArea()
+        .ignoresSafeArea(.container, edges: .top)
+        .toolbar {
+            ToolbarItem(placement: .keyboard) {
+                Button(action: focusNextField) {
+                    Image(systemName: "chevron.down")
+                }
+            }
+            ToolbarItem(placement: .keyboard) {
+                Button("Done") {
+                    focusedInput = nil
+                }
+            }
+        }
+    }
+    
+    private var logoBanner: some View {
+        VStack {
+            Image(.logo)
+                .resizable()
+                .frame(width: 150, height: 150)
+                .padding(50)
+                .background(.accent)
+        }
+        .frame(maxWidth: .infinity)
+        .background(.accent)
+    }
+    
+    private var emailTextField: some View {
+        CustomTextField(
+            text: .constant(""),
+            title: "Email",
+            placeholder: "example@email.com"
+        )
+    }
+    
+    private var passwordTextField: some View {
+        CustomTextField(
+            text: .constant(""),
+            title: "Password",
+            placeholder: "•••••••••••",
+            secure: true
+        )
+    }
+    
+    private var loginButton: some View {
+        Button("Login") {
+            
+        }
+        .padding(.vertical, 20)
+        .foregroundStyle(.white)
+        .frame(maxWidth: .infinity)
+        .background(.accent)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    
+    private var signupButton: some View {
+        Button("Don't have an account? Sign up") {
+            
+        }
+        .fontWeight(.medium)
+        .frame(maxWidth: .infinity)
     }
 }
 
+private extension LoginView {
+    private enum Field: Int, CaseIterable {
+        case email, password
+    }
+    
+    private func focusNextField() {
+        focusedInput = focusedInput.map {
+            Field(rawValue: $0.rawValue + 1) ?? .email
+        }
+    }
+}
 #Preview {
     LoginView()
 }
